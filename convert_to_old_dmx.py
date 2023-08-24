ac8bf37e3571fc389468f135cd58cd644ca224c9
RobertPoncelet <robert.poncelet@talk21.com>
2023-08-24 23:18:40 +0100

    Add --encoding argument to dmx conversion script

--- convert_to_old_dmx.py
+++ convert_to_old_dmx.py
@@ -3,9 +3,10 @@ from . import datamodel
 
 parser = argparse.ArgumentParser()
 parser.add_argument("path")
+parser.add_argument("--encoding", default="binary")
 args = parser.parse_args()
 
 dm = datamodel.load(args.path)
 base, extension = os.path.splitext(args.path)
 out_path = base + "_converted" + extension
-dm.write("binary", 2)
+dm.write(out_path, args.encoding, 1)
